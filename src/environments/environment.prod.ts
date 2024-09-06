7c7bfd61764d952699a7c395240181ba4503e9b4
Sandeep Singh <165636087+sandeep-ivaninfotech@users.noreply.github.com>
2024-09-06 13:28:32 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://hrmlix-ind-company-backend-service-ueusq7jtpq-el.a.run.app',
-    BASEHREF: 'api/company-backend-service',
-    ASSET_URL: '',
-    APPNAME: 'The Seamless HR Solution',
-    DATATABLELENGTH: 20,
-    DATATABLELENGTHCHANGEMENU: [1, 10, 20, 50, 75, 100],
-    USERAUTH: '',
-    GOOGLEMAPKEY: 'AIzaSyCJh98m3Q4lDtUrn21uG8p-bSTc0pZvxOI',
-    RAZORPAYKEY: 'rzp_test_MhGoAHitNBkeU0',
-    RAZORPAYCOMPANYNAME: 'HRMlix',
-    RAZORPAYCOMPANYLOGO: 'https://cdn3.iconfinder.com/data/icons/inficons-currency-set/512/rupee-512.png',
-    RAZORPAYCURRENCY: 'INR'
+    apiUrl: '/',
+    BASEHREF: '/',
   };
   
